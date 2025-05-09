ee4db0af3c224f35933af95a7c3279d3d212fc73
tareas <tareasrojano@gmail.com>
2025-05-09 11:28:48 -0600

    09-10 modulos

---
+++ html00/xM.js
@@ -0,0 +1,12 @@
+import {y} from "./yM.js"
+
+function x(params) {
+    y("x")
+    console.log("x: " + params)
+    return "fin x"
+}
+
+function xx(params) {
+    return "fin xx"
+}
+export {x, xx}
